68c4fe391f56d7cf9de32567acd74ad293f6fd5a
okilonet1 <okilonet1@gmail.com>
2022-03-05 02:47:36 +0100

    correct

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
-import React, { useLayoutEffect, useState } from 'react';
-
+import React from 'react';
 import './App.css';
 import sketch from './sketch';
 import p5 from 'p5';
-
-
-function useWindowSize() {
-  const [size, setSize] = useState([0, 0]);
-  useLayoutEffect(() => {
-    function updateSize() {
-      setSize([window.innerWidth, window.innerHeight]);
-    }
-    window.addEventListener('resize', updateSize);
-    updateSize();
-    return () => window.removeEventListener('resize', updateSize);
-  }, []);
-  return size;
-}
-
-function ShowWindowDimensions(props) {
-  const [width, height] = useWindowSize();
-  return <span>Window size: {width} x {height}</span>;
-}
 
 const TEXT = 'Happy Birthday MOM';
 
