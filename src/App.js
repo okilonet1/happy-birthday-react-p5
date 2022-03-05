cdea7c08134b12672e9694364cd652cc690b68e3
okilonet1 <okilonet1@gmail.com>
2022-03-05 01:47:43 +0100

    resize update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,26 @@ import './App.css';
 import sketch from './sketch';
 import p5 from 'p5';
 
+import React, { useLayoutEffect, useState } from 'react';
+
+function useWindowSize() {
+  const [size, setSize] = useState([0, 0]);
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+  return size;
+}
+
+function ShowWindowDimensions(props) {
+  const [width, height] = useWindowSize();
+  return <span>Window size: {width} x {height}</span>;
+}
+
 const TEXT = 'Happy Birthday MOM';
 
 const App = () => {
